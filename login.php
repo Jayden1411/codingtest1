740a6e2f9e3e502f6e2f846c59fcd2012926dc34
Jayden1411 <66936265+Jayden1411@users.noreply.github.com>
2022-12-03 14:56:39 +0200

    Update login.php

--- login.php
+++ login.php
@@ -205,11 +205,7 @@
 					<div class="row mb-3">
 					    <div class="col-md-6 offset-md-4">
 						<div class="form-check">
-						    <input class="form-check-input" type="checkbox" name="remember" id="remember" >
-
-						    <label class="form-check-label" for="remember"
-							Remember Me
-						    </label>
+						
 						</div>
 					    </div>
 					</div>
