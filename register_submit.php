ee49b4d2aa6cf1ee329b9db69d0d6019ea0c9db8
Jayden1411 <66936265+Jayden1411@users.noreply.github.com>
2022-12-04 08:45:09 +0200

    Update register_submit.php

--- register_submit.php
+++ register_submit.php
@@ -1,38 +1,31 @@
 <?php	
-	extract($_POST);	
-	include "connect.php";
-				
-	$flag=1;
-	$query = "select name from users where name='$name' and email='$email'";	
-	$result=$conn->query ("$query") or die ("Error in query: $off_sql".mysqli_error($conn));
-		
-	if(mysqli_num_rows($result)>0)
-		{
-		$msg= "User name or email already exist. <br>";
-		$flag=0;
-		
-		}
-
-	if($password !=$password_confirmation)
-		{
-		$msg= "Passwords not the same <br>";
-		$flag=0;
-		}
-	if($flag==1)
-		{
-			$password=password_hash($password, PASSWORD_DEFAULT);	
-			mysqli_query($conn,"insert into users(name, password,email) values('$name', '$password', '$email')")or die ("Error in query failed to add user".mysqli_error($conn));
+	include "common.php";
+	if (!empty($_POST))
+	{ 	extract($_POST);
+		$dbh = Db::db_conn();
+		$validate= Register::userValidate($dbh, $name,$email,$password,$password_confirmation);
+		$checks=trim($validate);
+		if(empty($checks)){
+			$register= Register::userRegister($dbh, $name,$email,$password);
 			header('Location: index.php');
-		?>
-			<script language = "javascript" style = "text/javascript"> 
-				window.location = "home.php?user=<?php echo $user?>";	
-			</script>
-		<?php
 		}else{
-			echo"
+		echo"
 			<div class=' row col-sm-8 pt-10' style='text-align:center;border:0; bgcolor: white;'>			
 			<div class='alert alert-danger'>
-				<strong>Oops!</strong> $msg 
+				<strong>Oops!</strong> $validate
+			</div>
+			<div class='alert alert-danger'>
+			<a href='javascript:history.go(-1)'>
+			<button class='btn btn-danger btn-sm'>Go Back</button></a>
+			</div>
+			</div>
+			";		
+		}
+	}else{
+		echo"
+			<div class=' row col-sm-8 pt-10' style='text-align:center;border:0; bgcolor: white;'>			
+			<div class='alert alert-danger'>
+				<strong>Oops!Form submit failed</strong> 
 			</div>
 			<div class='alert alert-danger'>
 			<a href='javascript:history.go(-1)'>
